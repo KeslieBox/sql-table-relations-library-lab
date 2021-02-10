16aa7eb16dfcaca62399242235c90ce85aa8d5c0
KeslieBox <kesliebox@gmail.com>
2021-02-09 22:45:40 -0600

    commit tree 03ca037e6aebd32a451363afb2f25dc38681cfef

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,11 +24,10 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM series INNER JOIN authors
-  ON series.author_id = series.subgenre_id
+  FROM authors INNER JOIN series
+  ON series.author_id = authors.id
   INNER JOIN subgenres
-  ON subgenres.id = authors.id
-  ORDER BY authors.name"
+  ON series.subgenre_id = subgenres.id"
 
   # "SELECT authors.name, subgenres.name
   # FROM authors INNER JOIN authors
@@ -37,9 +36,17 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title 
+  FROM series INNER JOIN characters 
+  ON characters.author_id = series.author_id
+  ORDER BY species DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_id) AS number
+  FROM characters INNER JOIN character_books
+  ON character_books.character_id = characters.id
+  GROUP BY character_id
+  ORDER BY number DESC"
 end
